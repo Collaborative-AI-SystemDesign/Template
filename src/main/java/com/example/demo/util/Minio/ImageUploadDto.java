5d7d7016667672db85d42071dbb744256e57bdf0
724thomas <724thomas@gmail.com>
2025-05-25 19:14:13 +0900

    Minio, rdb, caching

---
+++ src/main/java/com/example/demo/util/Minio/ImageUploadDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.util.Minio;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+public class ImageUploadDto {
+    String imageId;
+    String url;
+}
